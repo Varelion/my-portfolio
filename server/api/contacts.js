9ec654dbff74aec36e5723ba2204814506cd53af
EightFiveZeroEight <business.8508@gmail.com>
2023-03-17 14:47:00 -0400

    Modify mailer

--- server/api/contacts.js
+++ server/api/contacts.js
@@ -63,7 +63,7 @@ router.post('/send', async (req, res, next) => {
     const recipientEmails = ['ighormisc@gmail.com', 'business.8508@gmail.com'];
     let senderEmail = email;
     // message content and subject line
-    const subjectLine = `Message from ${senderEmail}`;
+    const subjectLine = `Varelion Message from ${senderEmail}`;
 
     // setup email data with unicode symbols
     let mailOptions = {
@@ -71,7 +71,10 @@ router.post('/send', async (req, res, next) => {
       to: recipientEmails.join(','),
       subject: subjectLine,
       text: message,
-      html: `<p>${message}</p>`,
+      html: `<h2>Message from${senderEmail}, at the website varelion.com</h2>
+      <br>
+      <br>
+      <p>${message}</p>`,
     };
 
     // send mail with defined transport object
